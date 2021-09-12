bfebead4d09531f5486eef365d23a625b7d1dc02
Graham Catterall <grahamcatterall@Grahams-MacBook-Air-2.local>
2021-09-12 19:03:54 +0100

    Song selected saves in redux state

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Some Say', duration: '4:32' },
+    { title: 'My Happy', duration: '6:50' },
+    { title: 'My My My', duration: '3:25'}
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
